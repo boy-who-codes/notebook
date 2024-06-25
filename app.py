86d39b4086b857b36676ae1fd6fb3cf1bc28ae1d
Rahul Sinha <48615211+boy-who-codes@users.noreply.github.com>
2024-06-25 11:54:51 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template
 from notebook import notebookapp
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    # Start notebook server
+    nb_app = notebookapp.NotebookApp.instance()
+    nb_app.initialize([])
+    nb_app.start()
+
+    # Get notebook URL
+    notebook_url = nb_app.connection_url
+    return render_template('index.html', notebook_url=notebook_url)
 
 if __name__ == '__main__':
     app.run(debug=True)
